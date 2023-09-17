3d4809e228e1a7ed3b85629fccdb96a8c4bc5d40
Lauren Morgan <rg326@live.com>
2023-09-16 23:12:40 -0400

    added bigo_worstcase file

---
+++ sb_python_dsa/bigo_worstcase.py
@@ -0,0 +1,7 @@
+## Notes
+
+# Omega - Best case scenario
+
+# Theta - Average case scenario
+
+# Omicron - "Big O" - Worst case scenario. When you are talking about big O, you are ALWAYS talking about worst case.
