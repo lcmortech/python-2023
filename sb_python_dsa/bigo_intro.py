6e9aa9b8efbfcfa0f9b4fd70a6ca872cfe24da27
Lauren Morgan <rg326@live.com>
2023-09-16 23:07:30 -0400

    new dir sb_python_dsa init

---
+++ sb_python_dsa/bigo_intro.py
@@ -0,0 +1,8 @@
+## Notes
+
+# Big O is a way of comparing two sets of code and how efficient they run
+
+# Time Complexity - NOT measured in time, but num of operations it takes to complete a task
+
+# Space Complexity - The code might take less time (less operations), but takes up more memory space
+
