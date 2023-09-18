067440dea6985aad1817fded75ff338beef2cc7b
Lauren Morgan <rg326@live.com>
2023-09-17 23:01:28 -0400

    added bigo_dropconstants.py, edited bigo_oofn.py

---
+++ sb_python_dsa/bigo_dropconstants.py
@@ -0,0 +1,9 @@
+# First rule for simplifying: drop constants (O(2n) -> O(n))
+
+def print_items(n):
+    for i in range(n):
+        print(i)
+    for j in range(n):
+        print(j)
+        
+print_items(10)
--- sb_python_dsa/bigo_oofn.py
+++ sb_python_dsa/bigo_oofn.py
@@ -1,3 +1,7 @@
 def print_items(n):
     for i in range(n):
         print(i)
+
+# O(n)
+
+print_items(2)
