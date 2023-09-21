e320e57b7e2524343a6a61f87ca26982b95d4220
Lauren Morgan <rg326@live.com>
2023-09-20 23:29:36 -0400

    add bigo_lists.py

---
+++ sb_python_dsa/bigo_lists.py
@@ -0,0 +1,8 @@
+my_list = [11, 3, 23, 7]
+my_list.append(17) #O(1)
+my_list.pop() #O(1)
+my_list.pop(0) # O(n) because you pop it from the start and have to move all the other indices
+my_list.insert(0,11) #O(n)
+
+# removing and adding is O(n)
+# removing is only O(1) if it's at the end(no need to rearrange the other indices)
