02e77afc41267e1c72364b3da82691c0fab0f01e
Lauren Morgan <rg326@live.com>
2023-09-22 23:54:16 -0400

    added intro to sorting methods to bigo_wrapup.py

--- sb_python_dsa/bigo_wrapup.py
+++ sb_python_dsa/bigo_wrapup.py
@@ -1,5 +1,6 @@
 #Wrap Up
 
+# Runtime Terminologies
 # O(1) = 1 operation
 # O(log n) ~ 7
 # O(n) = 100
@@ -14,3 +15,18 @@
 # O(log n) = Divide and Conquer
 # O(n) = Proportional
 # O(n^2) = Loop within a Loop
+
+# Intro to different sorting methods and runtimes
+# Quicksort -  TC: O(n ^ 2) SC: O(log(n))
+# Mergesort - TC: O(n log (n)) SC: O(n)
+# Timsort - TC: O(n log (n)) SC: O(n)
+# Heapsort - TC: O(n log (n)) SC: O(1)
+# Bubble Sort - TC: O(n ^ 2) SC: O(1)
+# Insertion Sort - TC: O(n ^ 2) SC: O(1)
+# Selection Sort - TC: O(n ^ 2) SC: O(1)
+# Tree Sort - TC: O(n log (n))  SC: O(n)
+# Shell Sort - TC: O(nlog((n))^2) SC: O(1)
+# Bucket Sort - TC: O(n ^ 2) SC: O(n)
+# Radix Sort - TC: O(nk) SC: O(n+k)
+# Counting Sort -  TC: O(n+k) SC: O(k)
+# Cubesort - TC: O(n log (n)) SC: O(n)
