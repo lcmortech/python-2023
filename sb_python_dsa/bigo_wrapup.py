e6c5e233f247cda8b203457afa005d54c23704e9
Lauren Morgan <rg326@live.com>
2023-09-22 23:22:31 -0400

    added runtime terminology to bigo_wrapup.py

---
+++ sb_python_dsa/bigo_wrapup.py
@@ -0,0 +1,16 @@
+#Wrap Up
+
+# O(1) = 1 operation
+# O(log n) ~ 7
+# O(n) = 100
+# O(n^2) = 10,000
+
+# O(1) = 1
+# O(log n) ~ 10
+# O(n) = 1000
+# *O(n^2) = 1000000
+
+# O(1) = Constant
+# O(log n) = Divide and Conquer
+# O(n) = Proportional
+# O(n^2) = Loop within a Loop
