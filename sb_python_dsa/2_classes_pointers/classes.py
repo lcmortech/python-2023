aaa837d708179da8ff17a21160d702c54e7ece16
Lauren Morgan <rg326@live.com>
2023-09-23 23:46:44 -0400

    added example and notes to classes.py

--- sb_python_dsa/2_classes_pointers/classes.py
+++ sb_python_dsa/2_classes_pointers/classes.py
@@ -1,5 +1,22 @@
+# A constructor
+# If it has the self keyword, it is a method that i a part of a class
+# Color is the parameter of the constructor
 class Cookie:
-    def__init__(self, color)
-    self.color = color
+    def __init__(self, color):
+        self.color = color
+    
+    #getter
+    def get_color(self):
+        return self.get_color
+    
+    #setter
+    def set_color(self, color):
+        self.color = color
 
 cookie_one = Cookie('Green')
+cookie_two = Cookie('Blue')
+
+cookie_one.set_color('yellow')
+
+print('\nCookie one is now', cookie_one.get_color())
+print('Cookie two is still', cookie_two.get_color())
