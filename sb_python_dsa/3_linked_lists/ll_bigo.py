e4f64fcde7af4061ac104fd06e9633c0e9371c9d
Lauren Morgan <rg326@live.com>
2023-09-27 22:51:14 -0400

    added ll_underthehood.py

--- sb_python_dsa/3_linked_lists/ll_bigo.py
+++ sb_python_dsa/3_linked_lists/ll_bigo.py
@@ -1,2 +1,4 @@
 # Append - O(1) - the penultimate node and tail append to the last node 
-# Remove - O(n)
+# Prepend - O(1) - the first node and tail attach to the first node in the ll, while each following node points to next
+# Remove - O(n) - we change the old node's pointer to a new node
+# Search - O(n) - in order to find a node, we have to go through each node
