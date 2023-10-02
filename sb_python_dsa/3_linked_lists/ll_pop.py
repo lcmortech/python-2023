fbf531393094e7311f930bfb6c97d96db94c28a6
Lauren Morgan <rg326@live.com>
2023-10-01 23:57:10 -0400

    added high level overview of the pop method to ll_pop.py

---
+++ sb_python_dsa/3_linked_lists/ll_pop.py
@@ -0,0 +1,16 @@
+## POP METHOD
+#High-Level Overview
+# We need tail to point to something that is pointing to the node before, and before that, and so on
+# Set tail pointer to that pointer
+# Then set tail.next equal to None
+# Return the node popped at the end of the list
+
+#Bring in two variables pre and temp and set them equal to head
+# Remove head and tail for now
+#If temp.next is equal to not None (not at the end of the list)
+# Set pre equal to temp (already in the first iteration)
+# Set temp to temp.next
+# Remove temp and work with pre variable
+# Then set tail equal to pre
+# Set tail.next to None
+# Return temp
