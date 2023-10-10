717b69f212d54c5133b4e44212ccfa13f9bbeb31
Lauren Morgan <rg326@live.com>
2023-10-09 23:18:18 -0400

    added linsearch2.py

--- dsa_sandbox/linsearch1.py
+++ dsa_sandbox/linsearch1.py
@@ -4,15 +4,18 @@ def linearsearch(list, targ):
   for i in range(len(list)):
     if list[i] == targ:
       return i
-    #elif i != list[i]:
-      #print('Not in list')
-    else:
+    elif i != list[i]:
       i += 1
+    else:
+      print('Not in list')
  
 print(list1[-1])
 print(linearsearch(list1, 9))
 print(linearsearch(list1, 22))
 print(linearsearch(list1, 8))
 print(linearsearch(list1, 56))
+print(linearsearch(list1, 51))
 print(linearsearch(list1, 3))
 print(linearsearch(list1, 5))
+
+# Runtime: O(n)
---
+++ dsa_sandbox/linsearch2.py
@@ -0,0 +1,3 @@
+list2 = [2,6,4,9,7,20,1,8,34,22,11,56,3,5]
+
+def linearsearch2(list, targ):
