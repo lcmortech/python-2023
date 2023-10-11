417930a1760f71d32a9f658e924169a50bf0cd0a
Lauren Morgan <rg326@live.com>
2023-10-10 23:31:13 -0400

    added code to linsearch2.py

--- dsa_sandbox/linsearch2.py
+++ dsa_sandbox/linsearch2.py
@@ -1,3 +1,21 @@
 list2 = [2,6,4,9,7,20,1,8,34,22,11,56,3,5]
 
 def linearsearch2(list, targ):
+    searchMap = {}
+
+    for i, n in enumerate(list):
+        if n == targ:
+            return i
+        elif n != targ:
+            i += 1
+        else:
+            return "Not in list"
+
+print(list2[-1])
+print(linearsearch2(list2, 9))
+print(linearsearch2(list2, 22))
+print(linearsearch2(list2, 8))
+print(linearsearch2(list2, 56))
+print(linearsearch2(list2, 51))
+print(linearsearch2(list2, 3))
+print(linearsearch2(list2, 5))
