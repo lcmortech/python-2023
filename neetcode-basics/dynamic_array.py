cd1af69286b61befcd11d6c1dd6ffe4131e3cb3e
Lauren Morgan <rg326@live.com>
2023-10-12 23:53:40 -0400

    added class structure for dynamic array

---
+++ neetcode-basics/dynamic_array.py
@@ -0,0 +1,19 @@
+#CONSTRUCT A DYNAMIC ARRAY
+
+class DynamicArray:
+    def _init__(self, capacity, int):
+        self.capacity = capacity
+        self.arr = [0] * capacity
+    
+    def get(self, i): 
+        pass
+    def insert(self, i, val): 
+        pass
+    def pushback(self, val): 
+        pass
+    def resize(self):
+        pass
+    def get_size(self):
+        pass
+    def get_capacity(self) -> int:
+        return self.capacity
