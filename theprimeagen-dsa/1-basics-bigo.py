3006cdce77ef177b29e30cf5675d7cea5f6f646f
Lauren Morgan <rg326@live.com>
2023-10-17 11:54:54 -0400

    added dir theprimeagen-dsa

---
+++ theprimeagen-dsa/1-basics-bigo.py
@@ -0,0 +1,12 @@
+## NOTES
+
+# We use insertion sort for smaller subsets of data bc while its slower, being O(n^2), its faster than say quicksort which is O(logn), because the n is sufficiently small enough for the constant being dropped makes more of an impact
+
+# IMPORTANT CONCEPTS
+# Growth is with respect to the input
+# Constants are dropped
+# Big O is the "Upper Bound"
+# Worst case is usually the way we measure
+# base 2 log = O(log n) (log2base2 ?)
+
+# Fav algo is quicksort (ring buffer, or an array list if you dont need a fifo behavior)
