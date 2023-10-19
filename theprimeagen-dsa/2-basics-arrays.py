e9366541bd13e9172efdfe24bd1c76574510e254
Lauren Morgan <rg326@live.com>
2023-10-18 23:55:43 -0400

    added notes to 2-basics-array.py

---
+++ theprimeagen-dsa/2-basics-arrays.py
@@ -0,0 +1,8 @@
+## NOTES
+
+# When applicable:
+# I will pretend that javascript has arrays
+# I only have access to the .length property of an array.
+# No, I won't go full c neckbeard and require length to pass it.
+
+# Algorithms: The A in DSA
