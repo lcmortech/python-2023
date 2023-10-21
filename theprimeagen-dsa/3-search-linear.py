4d28547c06c1c216671acb40786eece6ceba60b5
Lauren Morgan <rg326@live.com>
2023-10-20 23:26:28 -0400

    added notes to 3-search-linear.py

--- theprimeagen-dsa/3-search-linear.py
+++ theprimeagen-dsa/3-search-linear.py
@@ -1,2 +1,5 @@
 ## NOTES
 # It's a very good practice to get into is being able to visualize the problem, discuss it with boxes and arrows, and then program it. Its definitely a core competency that will follow you for life.
+
+# What's the way something fails? If we search the entire length of the array and the item is not there
+# As your input grows so does the time it takes linearly
