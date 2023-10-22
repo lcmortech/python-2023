d63acd7df5cd61b57cc687a6cdab7f2f7bd1fe65
Lauren Morgan <rg326@live.com>
2023-10-21 23:05:36 -0400

    minor edit to notes in 3-search-linear.py

--- theprimeagen-dsa/3-search-linear.py
+++ theprimeagen-dsa/3-search-linear.py
@@ -1,10 +1,14 @@
 ## NOTES
-# It's a very good practice to get into is being able to visualize the problem, discuss it with boxes and arrows, and then program it. Its definitely a core competency that will follow you for life.
+# A very good practice to get into is being able to visualize the problem, discuss it with boxes and arrows, and program it. Its definitely a core competency that will follow you for life.
 
 # What's the way something fails? If we search the entire length of the array and the item is not there
 # As your input grows so does the time it takes linearly
+
+
 
 def linear_search(haystack, targ):
     for needle in haystack:
         if needle == targ:
             return True
+        
+
