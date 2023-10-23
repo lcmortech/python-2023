106632bafc2a433c274fdce201a5d30dac4ad042
Lauren Morgan <rg326@live.com>
2023-10-22 23:53:25 -0400

    added notes to 4-search-binary.py

--- theprimeagen-dsa/4-search-binary.py
+++ theprimeagen-dsa/4-search-binary.py
@@ -4,4 +4,4 @@
 # An important question you should always ask for your dataset: Is it ordered? If it is, there are new advantages you can take with that data
 # Lets say it this time. So how can we search this array?
 # First, let's come up with the algorithm together
-
+# The worst case scenario is when the value's not in the array and you will halve it over and over again until the end.
