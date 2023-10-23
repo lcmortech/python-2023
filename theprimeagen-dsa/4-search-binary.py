1d9dc784760c3b435518514cbcb4d988c012f043
Lauren Morgan <rg326@live.com>
2023-10-22 23:49:09 -0400

    added 4-search-binary.py

---
+++ theprimeagen-dsa/4-search-binary.py
@@ -0,0 +1,7 @@
+## NOTES
+
+# This second algorithm is a bit pof a doosy, but its definitely a basis for other algorithms we will encounter.
+# An important question you should always ask for your dataset: Is it ordered? If it is, there are new advantages you can take with that data
+# Lets say it this time. So how can we search this array?
+# First, let's come up with the algorithm together
+
