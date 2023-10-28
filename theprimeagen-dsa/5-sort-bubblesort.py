84cb8ee40aac155ff49a9bc14b680e13aa1da779
Lauren Morgan <rg326@live.com>
2023-10-27 23:10:36 -0400

    added notes to 5-sort-bubblesort.py

---
+++ theprimeagen-dsa/5-sort-bubblesort.py
@@ -0,0 +1,7 @@
+## NOTES
+
+# Yes, let's do bubble sort. Usually the first algorithm in almost every major source is insertion sort, but I find it so boring. Let's do bubble sort!
+# As my teacher once said...to the whiteboard!
+# Any x sub-i/ith poisiton in the array is going to be less than or equal to any i+1
+# starts in 0-eth position (first position) its gonna go to the end of the array. It goes to the +1 next to it and asks, if I'm larger than you, let's swap positions.
+# A singular iteration will always produce the largest item in the last spot/position. We can go up to, but not include the last position
