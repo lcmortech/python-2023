661c5300505067d1d6595fdc5f2225a5b47f0485
Lauren Morgan <rg326@live.com>
2023-10-29 23:08:06 -0400

    added note to 5-sort-bubblesort.py

--- theprimeagen-dsa/5-sort-bubblesort.py
+++ theprimeagen-dsa/5-sort-bubblesort.py
@@ -6,6 +6,8 @@
 # starts in 0-eth position (first position) its gonna go to the end of the array. It goes to the +1 next to it and asks, if I'm larger than you, let's swap positions.
 # A singular iteration will always produce the largest item in the last spot/position. We can go up to, but not include the last position
 # An array with one element is ALWAYS sorted
+# sort in place algorithm
+
 
 #O(n^2)
 def bubsort(list):
