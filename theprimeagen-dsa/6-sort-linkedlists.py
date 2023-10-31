39d44be3e7880047595f17bd25e0324581d8f992
Lauren Morgan <rg326@live.com>
2023-10-30 23:56:07 -0400

    added notes to 6-sort-linkedlists.py

--- theprimeagen-dsa/6-sort-linkedlists.py
+++ theprimeagen-dsa/6-sort-linkedlists.py
@@ -1,1 +1,11 @@
 ## NOTES
+
+# So what sucks about an array?
+# Deletion - can't delete but copy
+# Can't insert but can write
+# It's ungrowable
+
+# LINKED LISTS
+# Our first node-based data structure
+# The node consists of an item/value and a pointer(next) that points to the next node
+# the pointer contains a reference to another value or another node
