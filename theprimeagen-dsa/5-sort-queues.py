e4fc1f137f8de6aa43ba9e327016d30fef2fbb2b
Lauren Morgan <rg326@live.com>
2023-11-02 23:46:27 -0400

    added extra note to 5-sort-queues.py

--- theprimeagen-dsa/5-sort-queues.py
+++ theprimeagen-dsa/5-sort-queues.py
@@ -3,3 +3,4 @@
 # This is probably the most common data structure that I have implemented in many languages.
 # FIFO - First In First Out (like waiting in line)
 # To the whote board!
+# We use a singly linked list, we don't need  a doubly linked list
