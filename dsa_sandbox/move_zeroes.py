ddc4cc14811c0bdcee738bca8da84ccf99b86413
Lauren Morgan <rg326@live.com>
2023-11-12 23:19:11 -0500

    edited code in move_zeroes.py

--- dsa_sandbox/move_zeroes.py
+++ dsa_sandbox/move_zeroes.py
@@ -1,7 +1,11 @@
 def move_zeroes(nums):
     pt = len(nums)-1
 
+    for i,j in enumerate(nums):
+        print(i,j)
+
     for num in range(0, len(nums)-1):
+        print(num)
         if nums[num] == 0: #if the element at this index is equal to 0
             nums[num], nums[pt] = nums[pt], nums[num]
             pt -= 1
