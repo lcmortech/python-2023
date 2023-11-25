3fc544b193a347fc66bd75eb9a01f6aa804bed79
Lauren Morgan <rg326@live.com>
2023-11-24 23:17:42 -0500

    added notes to array buffer file

---
+++ theprimeagen-dsa/6-arrays-arraybuffers.py
@@ -0,0 +1,5 @@
+## NOTES
+
+# The diff between an array/list they don't use 0 as heasd or length as tail, like an array list. You can have an index-based head and an index-based tail
+# Just add +1 to the head or tail, which makes them O(1) operations
+# You can also use the modulo operato this.tail % len (typescript) gives you your index into the array
