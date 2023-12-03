89152229ccbde3252b6701480bc9683473b0f24d
Lauren Morgan <rg326@live.com>
2023-12-02 20:48:34 -0500

    added brute force test case for two sum

--- dsa_sandbox/two_sum.py
+++ dsa_sandbox/two_sum.py
@@ -1,12 +1,19 @@
 #NOTES
 
 #Brute Force
-def two_sum(list, t):
+def two_sum(nums, t):
 
-    new_list = []
-    for i in range(len(list)):
+    new_nums = []
+    for i in range(len(nums)):
         for j in range(i + 1, len(nums)):
             if nums[i] + nums[j] == t:
                 return [i, j]
             
-    return new_list
+    return new_nums
+
+#TEST
+
+test = two_sum([3,6,4,5,], 6)
+test2 = two_sum([3,6,4,5,], 10)
+print(test)
+print(test2)
