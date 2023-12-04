4eb7aaa2491a023bba4c7b2b38189ad1f31001ed
Lauren Morgan <rg326@live.com>
2023-12-03 23:20:09 -0500

    added hash map solution for two sum

--- dsa_sandbox/two_sum.py
+++ dsa_sandbox/two_sum.py
@@ -1,17 +1,30 @@
 #NOTES
 
-#Brute Force
-def two_sum(nums, t):
+#Brute Force O(n^2)
+# def two_sum(nums, t):
 
-    new_nums = []
-    for i in range(len(nums)):
-        for j in range(i + 1, len(nums)):
-            if nums[i] + nums[j] == t:
-                return [i, j]
+#     new_nums = []
+#     for i in range(len(nums)):
+#         for j in range(i + 1, len(nums)):
+#             if nums[i] + nums[j] == t:
+#                 return [i, j]
             
-    return new_nums
+#     return new_nums
 
-#TEST
+
+#Hashmap
+def two_sum(nums, t):
+    new_map = {}
+    count = len(nums)
+
+    for i in range(count):
+        diff = t - nums[i]
+        if diff in new_map:
+            return [new_map[diff], i]
+        
+    return []
+
+#TEST O(n)
 
 test = two_sum([3,6,4,5,], 6)
 test2 = two_sum([3,6,4,5,], 10)
