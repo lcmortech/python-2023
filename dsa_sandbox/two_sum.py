7584a4c3ec6efeb9307a0177bf2f58fcf5a0a012
Lauren Morgan <rg326@live.com>
2023-12-06 22:45:33 -0500

    added notes to conditional in hashmap solution for two sum

--- dsa_sandbox/two_sum.py
+++ dsa_sandbox/two_sum.py
@@ -18,8 +18,8 @@ def two_sum(nums, t):
     count = len(nums) #intiate a counter using the length of the inputted list
 
     for i in range(count): # for each index in the length of nums
-        diff = t - nums[i] # variable that is the difference between the target number from two items
-        if diff in new_map:
+        diff = t - nums[i] # variable that is the difference between the target number(vsl) from two items(val)
+        if diff in new_map: #if the diff of the target number is in the dictionary
             return [new_map[diff], i]
         
     return []
