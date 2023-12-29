baaa62cab0bedaff892f627817fefd09115c2e5f
Lauren <rg326@live.com>
2023-12-28 22:46:49 -0500

    added full implementation of class deque with built-in methods

--- neetcode-basics/double-queue.py
+++ neetcode-basics/double-queue.py
@@ -51,3 +51,21 @@
     #def __init__(self):
        # self.head = self.tail = None
 
+class Deque:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def addFront (self, item):
+        self.items.append(item)
+    
+    def removeFront(self):
+        return self.items.pop()
+
+    def removeRear(self):
+        return self.items.pop(0)
+
+    def size (self):
+        return len(self.items)
