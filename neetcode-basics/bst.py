93e3c4bab65a4e66f8cd6ed4534a5712c1968ebc
Lauren <rg326@live.com>
2024-01-12 23:41:48 -0500

    added additional nodes to binary search tree in bst.py

--- neetcode-basics/bst.py
+++ neetcode-basics/bst.py
@@ -84,5 +84,10 @@ return root
 
 root = None
 root = insert(root, 8)
-
-
+root = insert(root, 3)
+root = insert(root, 1)
+root = insert(root, 6)
+root = insert(root, 7)
+root = insert(root, 10)
+root = insert(root, 14)
+root = insert(root, 4)
