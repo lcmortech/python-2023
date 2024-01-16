2cc1d1e9a6b638b369d2e409623405445a45c3a8
Lauren <rg326@live.com>
2024-01-15 23:19:47 -0500

    added function checkPrime to hash_table.py

--- neetcode-basics/hash_table.py
+++ neetcode-basics/hash_table.py
@@ -1,1 +1,18 @@
 # HASH TABLE DATA STRUCTURE (Programiz)
+
+hashTable = [[],] * 10
+
+def checkPrime(n):
+    if n == 1 or n == 0:
+        return 0
+
+    for i in range(2, n//2):
+        if n % i == 0:
+            return 0
+
+    
+    return 1
+
+def getPrime(n):
+    if n % 2 == 0:
+        n = n + 1
