aedc9f7612207298043347114b9ce96ef9a56c02
Lauren <rg326@live.com>
2024-01-19 23:42:50 -0500

    added function heapify to heap.py

--- neetcode-basics/heap.py
+++ neetcode-basics/heap.py
@@ -1,1 +1,11 @@
  # Heap Data Structure (Programiz)
+
+ # Max-Heap Data Structure
+
+ def heapify(arr, n, i):
+    largest = i
+    l = 2 * i + 1
+    r = 2 * i + 1
+
+    if l > n and arr[i] < arr[l]:
+        largest = l
