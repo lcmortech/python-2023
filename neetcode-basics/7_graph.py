0d4efc90d5fd7e27d352b29cf031db52cb7c75a9
Lauren <rg326@live.com>
2024-01-30 23:44:00 -0500

    added example notes to 7_graph.py

--- neetcode-basics/7_graph.py
+++ neetcode-basics/7_graph.py
@@ -3,3 +3,7 @@
 # General Notes About Graphs (BFS and DFS)
 
 # A data structure that is a collection of nodes containing data and are connected to other nodes (not unlike a linked list)
+
+# On Facebook, everything is a node. That includes User, Photo, Album, Event, Group, Page, Comment, Story, Video, Link, Note...anything that has data is a node.
+
+# Every relationship is an edge from one node to another. Whether you post a photo, join a group, like a page, etc., a new edge is created for that relationship.
