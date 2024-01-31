910c33545879c005a29ff4270eda7c8c7e920a45
Lauren <rg326@live.com>
2024-01-30 23:44:00 -0500

    added def notes to 7_graph.py

--- neetcode-basics/7_graph.py
+++ neetcode-basics/7_graph.py
@@ -1,3 +1,5 @@
 # Design a Graph Data Structure
 
 # General Notes About Graphs (BFS and DFS)
+
+# A data structure that is a collection of nodes containing data and are connected to other nodes (not unlike a linked list)
