179bdd0594d49712d56eb08bf77106b9012163b3
Lauren <rg326@live.com>
2024-01-31 23:51:59 -0500

    added notes on vertices and edges to 7_graph.py

--- neetcode-basics/7_graph.py
+++ neetcode-basics/7_graph.py
@@ -1,3 +1,4 @@
+
 # Design a Graph Data Structure
 
 # General Notes About Graphs (BFS and DFS)
@@ -7,3 +8,8 @@
 # On Facebook, everything is a node. That includes User, Photo, Album, Event, Group, Page, Comment, Story, Video, Link, Note...anything that has data is a node.
 
 # Every relationship is an edge from one node to another. Whether you post a photo, join a group, like a page, etc., a new edge is created for that relationship. All of facebook is then a collection of these nodes and edges. This is because facebook uses a graph data structure to store its data.
+
+# More precisely, a graph is a data structure (V,E) that consists of:
+# A collection of vertices (V)
+# A collection of edges (E), represented as ordered pairs of vertices
+# (U,V)
