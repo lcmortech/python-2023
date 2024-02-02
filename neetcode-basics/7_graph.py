4557bd127033679935064778409f174eac380436
Lauren <rg326@live.com>
2024-02-01 23:34:15 -0500

    added graph terminology to 7_graph.py

--- neetcode-basics/7_graph.py
+++ neetcode-basics/7_graph.py
@@ -18,4 +18,7 @@
 # E = {{0,1}, {0,2}, {1,2}}
 # G = {V, E}
 
-
+# Graph Terminology
+# Adjacency: A vertex is said to be adjacent to another vertex if there is an edge connecting them. Vertices 2 and 3 are not adjacent because there is no edge between them.
+# Path: A sequence of edges that allows you to go from vertex A to vertex B is called a path. 0-1, 1-2, and 0-2 are paths from vertex 0 to vertex 2.
+# Directed Graph: A graph in which an edge (u,v) doesn't necessarily mean that there is an edge (v, u) as well. The edges in such a graph are represented by arrows to show the direction of the edge.
