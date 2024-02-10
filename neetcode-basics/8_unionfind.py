faecb7b1d9aa69113f16923809d2f93a46ac6ef1
Lauren <rg326@live.com>
2024-02-09 23:50:43 -0500

    added def notes contd. to 8_unionfind.py

--- neetcode-basics/8_unionfind.py
+++ neetcode-basics/8_unionfind.py
@@ -3,6 +3,9 @@
 # Any minimum spanning tree algorithm revolves around checking if adding an edge creates a loop or not. 
 
 # The most common way to find this out is an algorithm called Union Find.
+
+# The Union-Find algorithm divides the vertices into clusters and allows us to check if two vertices belong to the same cluster or not and hence decide whether adding an edge creates a cycle.
+
 # Setting up the Graph class
 
 
