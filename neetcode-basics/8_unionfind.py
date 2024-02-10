274e261eaca26bb7913eb71d7ec206e2ce27d6af
Lauren <rg326@live.com>
2024-02-09 23:50:43 -0500

    added def fnotes to 8_unionfind.py

--- neetcode-basics/8_unionfind.py
+++ neetcode-basics/8_unionfind.py
@@ -1,5 +1,8 @@
 # Disjoint Set/Union Find/Kruskal's Algorithm (Programiz)
 
+# Any minimum spanning tree algorithm revolves around checking if adding an edge creates a loop or not. 
+
+# The most common way to find this out is an algorithm called Union Find.
 # Setting up the Graph class
 
 
