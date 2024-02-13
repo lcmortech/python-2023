b9f4c633747f1a031aabb6b829008a9cdd654b96
Lauren <rg326@live.com>
2024-02-12 23:31:36 -0500

    added function kruskal_algo to 8_unionfind.py

--- neetcode-basics/8_unionfind.py
+++ neetcode-basics/8_unionfind.py
@@ -50,3 +50,25 @@ class Graph:
         else:
             parent[yroot] = xroot
             rank[xroot] += 1
+
+  #  Applying Kruskal algorithm
+    def kruskal_algo(self):
+        result = []
+        i, e = 0, 0
+        self.graph = sorted(self.graph, key=lambda item: item[2])
+        parent = []
+        rank = []
+        for node in range(self.V):
+            parent.append(node)
+            rank.append(0)
+        while e < self.V - 1:
+            u, v, w = self.graph[i]
+            i = i + 1
+            x = self.find(parent, u)
+            y = self.find(parent, v)
+            if x != y:
+                e = e + 1
+                result.append([u, v, w])
+                self.apply_union(parent, rank, x, y)
+        for u, v, weight in result:
+            print("%d - %d: %d" % (u, v, weight))        
