d776e3abc92547db85966c3ddbd8e2fa3d1f603f
Lauren <rg326@live.com>
2024-02-13 22:57:45 -0500

    added def notes to 9_segtree.py

---
+++ neetcode-basics/9_segtree.py
@@ -0,0 +1,3 @@
+# SEGMENT TREE DATA STRUCTURE (Geek4Geeks)
+
+# A Segment Tree is a data structure that allows programmers to solve range queries over the given array effectively and to modifying the array values. Basically, Segment Tree is a very flexible and efficient data structure and a large number of problems can be solved with the help of segment trees.
