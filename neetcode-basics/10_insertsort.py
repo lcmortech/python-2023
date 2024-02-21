bc318b4758161995f330a338fa767106b94739f7
Lauren <rg326@live.com>
2024-02-20 22:43:22 -0500

    added pseudo code example of insertion sort to 10_insertsort.py

--- neetcode-basics/10_insertsort.py
+++ neetcode-basics/10_insertsort.py
@@ -7,3 +7,14 @@
 # We assume that the first card is already sorted then, we select an unsorted card. If the unsorted card is greater than the card in hand, it is placed on the right otherwise, to the left. In the same way, other unsorted cards are taken and put in their right place.
 
 # A similar approach is used by insertion sort.
+
+# Pseudo Code Example
+insertionSort(array)
+  mark first element as sorted
+  for each unsorted element X
+    'extract' the element X
+    for j <- lastSortedIndex down to 0
+      if current element j > X
+        move sorted element to the right by 1
+    break loop and insert X here
+end insertionSort
