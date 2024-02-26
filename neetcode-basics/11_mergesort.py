a0e29529249f8a57dd9dba17efcc639a4622e383
Lauren <rg326@live.com>
2024-02-25 23:09:28 -0500

    fetched/pull from master, added def notes to 11_mergesort.py

--- neetcode-basics/11_mergesort.py
+++ neetcode-basics/11_mergesort.py
@@ -2,3 +2,7 @@
 
 # emergency - hospital stay 1 (friday)
 # emergency - hospital stay 2 (saturday)
+
+# [fetched, pull]
+
+# Merge Sort is one of the most popular sorting algorithms that is based on the principle of Divide and Conquer Algorithm.
