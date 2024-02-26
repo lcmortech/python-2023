e906fe5f35f053248932cfd760006cc7a1cbfbb0
Lauren <rg326@live.com>
2024-02-25 23:09:28 -0500

    added process notes for merge sort to 11_mergesort.py

--- neetcode-basics/11_mergesort.py
+++ neetcode-basics/11_mergesort.py
@@ -6,3 +6,5 @@
 # [fetched, pull]
 
 # Merge Sort is one of the most popular sorting algorithms that is based on the principle of Divide and Conquer Algorithm.
+
+# Using merge sort, a problem is divided into multiple sub-problems. Each sub-problem is solved individually. Finally, sub-problems are combined to form the final solution.
