86bc15292574616fd2dd051369c4aa253e3991a6
Lauren <rg326@live.com>
2024-02-26 23:00:56 -0500

    added notes on divide and conquer method to 11_mergesort.py

--- neetcode-basics/11_mergesort.py
+++ neetcode-basics/11_mergesort.py
@@ -8,3 +8,7 @@
 # Merge Sort is one of the most popular sorting algorithms that is based on the principle of Divide and Conquer Algorithm.
 
 # Using merge sort, a problem is divided into multiple sub-problems. Each sub-problem is solved individually. Finally, sub-problems are combined to form the final solution.
+
+# THE DIVIDE AND CONQUER TECHNIQUE
+
+# Using the Divide and Conquer technique, we divide a problem into subproblems. When the solution to each subproblem is ready, we 'combine' the results from the subproblems to solve the main problem.
