377fc2d545013a370b45ce6ebce24f7a22598044
Lauren <rg326@live.com>
2024-03-05 22:04:07 -0500

    added example of mergesort in python to 11_mergesort.py

--- neetcode-basics/11_mergesort.py
+++ neetcode-basics/11_mergesort.py
@@ -79,4 +79,66 @@ MergeSort(A, p, r):
 # k maintains the current index of A[p..q], starting at p.
 # Until we reach the end of either L or M, pick the larger among the elements from L and M and place them in the correct position at A[p..q]
 # When we run out of elements in either L or M, pick up the remaining elements and put in A[p..q]
-# In code, this would look like:
+# In code, this would look like: (See programiz for example in Java)
+
+# =========STEPS====
+
+
+
+# ==========END======
+
+# MergeSort in Python
+
+def mergeSort(array):
+    if len(array) > 1:
+
+        #  r is the point where the array is divided into two subarrays
+        r = len(array)//2
+        L = array[:r]
+        M = array[r:]
+
+        # Sort the two halves
+        mergeSort(L)
+        mergeSort(M)
+
+        i = j = k = 0
+
+        # Until we reach either end of either L or M, pick larger among
+        # elements L and M and place them in the correct position at A[p..r]
+        while i < len(L) and j < len(M):
+            if L[i] < M[j]:
+                array[k] = L[i]
+                i += 1
+            else:
+                array[k] = M[j]
+                j += 1
+            k += 1
+
+        # When we run out of elements in either L or M,
+        # pick up the remaining elements and put in A[p..r]
+        while i < len(L):
+            array[k] = L[i]
+            i += 1
+            k += 1
+
+        while j < len(M):
+            array[k] = M[j]
+            j += 1
+            k += 1
+
+
+# Print the array
+def printList(array):
+    for i in range(len(array)):
+        print(array[i], end=" ")
+    print()
+
+
+# Driver program
+if __name__ == '__main__':
+    array = [6, 5, 12, 10, 9, 1]
+
+    mergeSort(array)
+
+    print("Sorted array is: ")
+    printList(array)
