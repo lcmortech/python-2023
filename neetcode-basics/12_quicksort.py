17347028f31747d60fc2b2ffcfc6b13b309a2c90
Ren <rg326@live.com>
2024-03-12 23:23:03 -0400

    added step 1 for quicksort algo to 12_quicksort.py

--- neetcode-basics/12_quicksort.py
+++ neetcode-basics/12_quicksort.py
@@ -7,3 +7,10 @@
 # While dividing the array, the pivot element should be positioned in such a way that elements less than pivot are kept on the left side and elements greater than pivot are on the right side of the pivot.
 # The left and right subarrays are also divided using the same approach. This process continues until each subarray contains a single element.
 # At this point, elements are already sorted. Finally, elements are combined to form a sorted array.
+
+#====
+# Working of Quicksort Algorithm
+
+# 1. Select the Pivot Element
+# There are different variations of quicksort where the pivot element is selected from different positions. Here, we will be selecting the rightmost element of the array as the pivot element.
+
