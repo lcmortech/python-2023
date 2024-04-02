55ffa6da35532fb7406daa430df61c4ab9727ad3
Ren <rg326@live.com>
2024-04-01 23:52:23 -0400

    added notes on string data type to proj2_tipcalc.py

--- yu_python100/proj2_tipcalc.py
+++ yu_python100/proj2_tipcalc.py
@@ -1,3 +1,10 @@
 # Project 2: TIP CALCULATOR
 
 # Data Types, Numbers, Operations, Type Conversion, f-Strings
+
+# Lesson 1: Primitive Data Types
+
+# String
+print(len("Hello"))
+# String Subscripting
+print("Hello"[0]) # //"H"
