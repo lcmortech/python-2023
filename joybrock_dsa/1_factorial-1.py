1aec1ce6efa76c34b68700d6af82e46042212089
Lauren <rg326@live.com>
2024-09-24 19:06:30 -0400

    added recursive method to problem for 1_factorial-1.py

--- joybrock_dsa/1_factorial-1.py
+++ joybrock_dsa/1_factorial-1.py
@@ -2,12 +2,25 @@
 # factorials only involve positive integers
 
 # iterative method
-def iterative_factorial(n):
+def iterative_factorial(num):
     fact = 1
-    for i in range(2, n + 1):
-        fact *= i # fact = fact * i
+    for i in range(2, num + 1):
+        fact *= i #fact = fact * i
     return fact
 
 print(iterative_factorial(5))
 
 # recursive method
+# last in, first out (LIFO)
+
+def recur_factorial(num):
+    if num == 1:
+        return num
+    else:
+        fact = recur_factorial(num - 1)
+        fact *= num #fact = fact * num
+
+    return fact
+
+
+    
