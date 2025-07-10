d08bcb3b0ca2bd42a32b2c372085744254b8b586
Lauren <rg326@live.com>
2025-07-09 23:50:26 -0400

    added programming exercises to ch1.py

--- zelle/ch1.py
+++ zelle/ch1.py
@@ -1,3 +1,17 @@
 # Ch1 - Computers and Programs
 
 #Multiple Choice: 1. a, 2. d, 3. d, 4. a, 5. b, 6. b, 7. c, 8. b, 9. b, 10. d
+
+#Programming Exercises
+print ("Hello, world! ")
+print("Hello", "world!")
+print (3)
+print(3.0)
+print (2 + 3)
+print(2. 0 + 3. 0)
+print (II 2 II + II 3 II )
+print ( "2 + 3 =", 2 + 3)
+print(2 * 3)
+print (2 ** 3)
+print (7 I 3)
+#print(?// 3)
