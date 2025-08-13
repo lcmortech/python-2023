59bf5ddab1a4e0d527a40d0a88c8a475cbfc12c0
Lauren <rg326@live.com>
2025-08-12 23:48:45 -0400

    added notes to button widget in tk_button.py in guis

--- guis/easy-steps/tk_button.py
+++ guis/easy-steps/tk_button.py
@@ -1,4 +1,8 @@
 from tkinter import *
 
+#created window, specified a title
 window = Tk()
 window.title("Button Example (widget)")
+
+#exits the program when clicked
+btn_end = Button(window, text = "Close", command = exit)
