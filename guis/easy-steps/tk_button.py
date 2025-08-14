7e742303b9d20c9f83180fbdb27a935700d7795a
Lauren <rg326@live.com>
2025-08-13 23:37:42 -0400

    added bg mode toggle to button in tk_button.py

--- guis/easy-steps/tk_button.py
+++ guis/easy-steps/tk_button.py
@@ -6,3 +6,10 @@ window.title("Button Example (widget)")
 
 #exits the program when clicked
 btn_end = Button(window, text = "Close", command = exit)
+
+#toggles window bg color
+def tog():
+    if window.cget("bg") == "yellow":
+        window.configure(bg = "gray")
+    else:
+        window.configure(bg = "yellow")
